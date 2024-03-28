e34ff37094d38e3b6e51cfa685d1abaa54fbd648
TheFeish <113859423+TheFeish@users.noreply.github.com>
2024-03-28 10:26:20 +0200

    Create DllApp.cpp

---
+++ DllApp/DllApp.cpp
@@ -0,0 +1,15 @@
+#include "../Dll/Dll.h"
+
+int main()
+{
+    auto start = std::chrono::high_resolution_clock::now();
+    F2("test", "M-Su,18:00-20:00");
+    F3();
+    for (int F = -2; F <= 2; F++) {
+        F4(F, -(studentoId % 19), studentoId % 25, studentoId / 1000000000000.0);
+        F5(F);
+    }
+    F6();
+    F1(start);
+    std::cin.get();
+}
