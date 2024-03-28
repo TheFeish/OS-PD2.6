e184745c4e030cdcb5423bcbfec5128d816574bb
TheFeish <113859423+TheFeish@users.noreply.github.com>
2024-03-28 10:25:28 +0200

    Dll files added

---
+++ Dll/Dll.h
@@ -0,0 +1,40 @@
+#pragma once
+#ifndef _DLL_H_
+#define _DLL_H_
+
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+#include <chrono>
+
+#ifdef DLL_EXPORTS
+#define DLL_API __declspec(dllexport)
+#else
+#define DLL_API __declspec(dllimport)
+#endif
+
+const int studentoId = 1613226;
+
+extern "C"
+DLL_API void F1(std::chrono::steady_clock::time_point);
+
+extern "C"
+DLL_API void F2(std::string, std::string);
+
+extern "C"
+DLL_API void F3();
+
+extern "C"
+DLL_API void F4(float, float, float, float);
+
+extern "C"
+DLL_API void F5(float);
+
+extern "C"
+DLL_API void F6();
+
+#endif
